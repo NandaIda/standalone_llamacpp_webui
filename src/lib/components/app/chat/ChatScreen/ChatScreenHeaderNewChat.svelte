0dbf93dd9433ad3eb9a1909432932d7d07b551ca
YogiIdaBagus <suananda920@live.com>
2025-12-02 12:10:17 +0800

    add new conversation button at the top for each conversation

---
+++ src/lib/components/app/chat/ChatScreen/ChatScreenHeaderNewChat.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { SquarePen } from '@lucide/svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="pointer-events-auto flex items-center space-x-2">
+	<Button variant="ghost" size="sm" onclick={() => goto('?new_chat=true#/')}>
+		<SquarePen class="h-4 w-4" />
+	</Button>
+</div>
